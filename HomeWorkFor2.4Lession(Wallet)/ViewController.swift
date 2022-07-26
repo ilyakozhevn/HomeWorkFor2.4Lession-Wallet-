e63d44959856d354c20248086595011587ecfc10
Fedor Eronin <eroninf@icloud.com>
2022-07-26 19:50:49 +0300

    Initial Commit

---
+++ HomeWorkFor2.4Lession(Wallet)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkFor2.4Lession(Wallet)
+//
+//  Created by Федор Еронин on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
